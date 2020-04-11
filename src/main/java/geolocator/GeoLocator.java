a7fe075d003df0dbcc74b2d66bd8bee3bec5a93d
Hinal Jogani <hinaljogani02@gmail.com>
2020-04-11 20:07:04 +0200

    Addedslf4j and log4j

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -9,6 +9,8 @@ import com.google.gson.Gson;
 import com.google.common.net.UrlEscapers;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class for obtaining geolocation information about an IP address or host
@@ -21,6 +23,7 @@ public class GeoLocator {
      * URI of the geolocation service.
      */
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
+    private static Logger logger = LoggerFactory.getLogger(GeoLocator.class);
 
     private static Gson GSON = new Gson();
 
@@ -36,6 +39,7 @@ public class GeoLocator {
      * @throws IOException if any I/O error occurs
      */
     public GeoLocation getGeoLocation() throws IOException {
+        logger.debug("Gets Geolocation");
         return getGeoLocation(null);
     }
 
@@ -52,20 +56,27 @@ public class GeoLocator {
         URL url;
         if (ipAddrOrHost != null) {
             ipAddrOrHost = UrlEscapers.urlPathSegmentEscaper().escape(ipAddrOrHost);
+            logger.debug("IPAddOrHost is evaluated: " + ipAddrOrHost);
             url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+            logger.info("URL is " + url);
         } else {
             url = new URL(GEOLOCATOR_SERVICE_URI);
+            logger.info("URL is " + url);
         }
         String s = IOUtils.toString(url, "UTF-8");
+        logger.debug("Converting url in UTF-8 encoding!");
         return GSON.fromJson(s, GeoLocation.class);
     }
     // CHECKSTYLE:OFF
     public static void main(String[] args) throws IOException {
         try {
             String arg = args.length > 0 ? args[0] : null;
+            logger.debug("Obtaining command line argument");
             System.out.println(new GeoLocator().getGeoLocation(arg));
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             System.err.println(e.getMessage());
+            logger.error("IOException Obtained: " + e.getMessage());
         }
     }
 
